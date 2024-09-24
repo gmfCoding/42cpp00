f82f0446014e6be89fa40eea577e1535820eccb9
Christopher Lovell <clovell@student.42adel.org.au>
2024-09-24 12:18:21 +0930

    Remove debug coded

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char** argv)
 		std::cout << "Too many / not enough arguments, there most only be one!\n";
 		return 1;
 	}
-	ScalarConverter::convert("''");
 	ScalarConverter::convert(argv[1]);
 	return 0;
 }
