cf424aa9de86887fdb6d4bbbdd0976a04c0b9fba
Christopher Lovell <gmfCoding@users.noreply.github.com>
2024-10-17 19:37:45 +1030

    wip ex00/09 Add CLI argument file

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,16 +1,22 @@
 #include "BitcoinExchange.hpp"
 #include <fstream>
 
-int main()
+int main(int argc, char **argv)
 {
 	std::ifstream file;
 	file.open("data.csv");
 
 	BitcoinExchange exchange(file);
 	file.close();
-
-	file.open("input.txt");
-	exchange.Process(file);
-	file.close();
-	return 0;
+	if (argc > 1)
+	{
+		file.open(argv[1]);
+		if (file.good())
+		{
+			exchange.Process(file);
+			file.close();
+			return 0;
+		}
+	}
+	std::cout << "Error: could not open file" << std::endl;
 }
