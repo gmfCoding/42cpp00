7830ee05fda8528d1e139ce7375d7b78701f0627
Christopher Lovell <gmfCoding@users.noreply.github.com>
2024-10-28 03:06:16 +1030

    wip ex02, remember to disable c23 and is_sorting

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -16,6 +16,20 @@ T containerAt(T it, int index)
 	return it;
 }
 
+static unsigned long jacobsthal_diff[] = {
+	2u, 2u, 6u, 10u, 22u, 42u, 86u, 170u, 342u, 682u, 1366u,
+	2730u, 5462u, 10922u, 21846u, 43690u, 87382u, 174762u, 349526u, 699050u,
+	1398102u, 2796202u, 5592406u, 11184810u, 22369622u, 44739242u, 89478486u,
+	178956970u, 357913942u, 715827882u, 1431655766u, 2863311530u, 5726623062u,
+	11453246122u, 22906492246u, 45812984490u, 91625968982u, 183251937962u,
+	366503875926u, 733007751850u, 1466015503702u, 2932031007402u, 5864062014806u,
+	11728124029610u, 23456248059222u, 46912496118442u, 93824992236886u, 187649984473770u,
+	375299968947542u, 750599937895082u, 1501199875790165u, 3002399751580331u,
+	6004799503160661u, 12009599006321322u, 24019198012642644u, 48038396025285288u,
+	96076792050570576u, 192153584101141152u, 384307168202282304u, 768614336404564608u,
+	1537228672809129216u, 3074457345618258432u, 6148914691236516864u
+};
+
 template <template <class, class> class Container = std::vector, class Allocator = std::allocator<int> >
 class PmergeMe
 {
@@ -192,6 +206,7 @@ private:
 		}
 
 		int mid = (low + high) / 2;
+		comparisons++;
 		if (item == *containerAt(chain.begin(), mid))
 			return mid + 1;
 		comparisons++;
@@ -210,24 +225,33 @@ private:
 
 	void InsertMainChain()
 	{
-		// for (PairContainer::iterator it = pairs.begin(); it != pairs.end(); it++)
-		// {
-		// 	InsertElement(it->b, chain);
-		// }
-		chain.insert(chain.begin(), pairs.begin()->b);
-		int loc, selected;
-		for (size_t i = 1; i < pairs.size(); ++i)
-		{
-			
-			selected = containerAt(pairs.begin(), i)->b;
-			if (selected < 0)
-				continue;
-			loc = BinarySearch(selected, 0, chain.size());
-			
-			chain.insert(containerAt(chain.begin(), loc), selected);
+		// Starting with the first element from the pairs
+		if (!pairs.empty()) {
+			chain.insert(chain.begin(), pairs.front().b);
+		}
+
+		for (size_t i = 1; i < pairs.size(); ++i) {
+			// Use Jacobsthal sequence to decide the segment size
+			if (i < sizeof(jacobsthal_diff) / sizeof(jacobsthal_diff[0])) {
+				size_t segment_size = jacobsthal_diff[i];
+				if (i + segment_size > pairs.size()) {
+					segment_size = pairs.size() - i; // Adjust segment size if it exceeds remaining pairs
+				}
+
+				// Perform binary search and insertion for each segment
+				for (size_t j = 0; j < segment_size && (i + j) < pairs.size(); ++j) {
+					int selected_value = containerAt(pairs.begin(), i + j)->b;
+					int loc = BinarySearch(selected_value, 0, chain.size());
+					chain.insert(containerAt(chain.begin(), loc), selected_value);
+				}
+				i += segment_size - 1; // Move the index forward by segment size
+			}
+		}
+
+		if (chain.size() != 8) {
+			std::cerr << "Unexpected chain size: " << chain.size() << std::endl;
 		}
 	}
-
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,6 +5,7 @@
 #include "Pair.hpp"
 #include "PmergeMe.hpp"
 #include <sstream>
+#include <ctime>
 
 int g_comparisons_count = 0;
 
@@ -50,6 +51,13 @@ void benchmark(T& t, int size, const std::string& container_name)
 	int comparisons;
 	std::clock_t start = std::clock();
 	typename T::IntContainer con = t.Sort(comparisons);
+	if (!std::is_sorted(con.cbegin(), con.cend()))
+	{
+		for (typename T::IntContainer::iterator it = con.begin(); it != con.end(); it++)
+			std::cout << *it << ' ';
+		std::cerr << "ERROR NOT SORTED" << std::endl;
+		exit(1);
+	}
 	std::clock_t end = std::clock();
 	double elapsed = static_cast<double>(end - start) / (CLOCKS_PER_SEC / 1000000.0);
 
@@ -58,7 +66,7 @@ void benchmark(T& t, int size, const std::string& container_name)
 		std::cout << *it << ' ';
 	std::cout << std::endl;
 	std::cout << "Time to process a range of " << size << " elements with" << container_name << " : " << elapsed << " us" << std::endl;
-	std::cout << std::endl;
+	std::cout << comparisons << std::endl;
 
 }
 
@@ -97,6 +105,7 @@ int main(int argc, char *argv[])
 		std::cout << std::endl;
 		benchmark(first, argc - 1, "std::vector");
 		benchmark(second, argc - 1, "std::list");
+
 	}
     catch (const char *errorMsg)
     {
@@ -104,203 +113,3 @@ int main(int argc, char *argv[])
     }
 	return 0;
 }
-
-// void createPairs(std::vector<int>& numbers, std::vector<Pair>& pairs, int& last)
-// {
-// 	for (std::size_t i = 0; i < numbers.size(); i++)
-// 	{
-// 		if (i % 2 != 0)
-// 			pairs.push_back(Pair(numbers[i - 1], numbers[i]));
-// 		else if (i == numbers.size() - 1)
-// 			last = numbers[i];
-// 	}
-// }
-
-// std::vector<Pair> merge(std::vector<Pair>& left, std::vector<Pair>& right)
-// {
-// 	std::vector<Pair> result;
-// 	std::size_t i = 0;
-// 	std::size_t j = 0;
-
-// 	while (i < left.size() && j < right.size())
-// 	{
-// 		g_comparisons_count++;
-// 		if (left[i].a < right[j].a)
-// 		{
-// 			result.push_back(left[i]);
-// 			i += 1;
-// 		}
-// 		else
-// 		{
-// 			result.push_back(right[j]);
-// 			j += 1;
-// 		}
-// 	}
-
-// 	result.insert(result.end(), left.begin() + i, left.end());
-// 	result.insert(result.end(), right.begin() + j, right.end());
-// 	return result;
-// }
-
-// std::vector<Pair> mergeSort(std::vector<Pair>& pairs)
-// {
-// 	if (pairs.size() <= 1)
-// 		return pairs;
-
-// 	int mid = pairs.size() / 2;
-
-// 	std::vector<Pair> leftHalf(pairs.begin(), pairs.begin() + mid);
-// 	std::vector<Pair> rightHalf(pairs.begin() + mid, pairs.end());
-
-// 	std::vector<Pair> sortedLeft = mergeSort(leftHalf);
-// 	std::vector<Pair> sortedRight = mergeSort(rightHalf);
-
-// 	return merge(sortedLeft, sortedRight);
-// }
-
-
-// void sortFive(std::vector<Pair>& pairs, int val)
-// {
-// 	g_comparisons_count++;
-// 	if (pairs.size() == 0)
-// 		pairs.insert(pairs.begin(), Pair(val));
-
-// 	else if (val < pairs[0].a)
-// 	{
-// 	g_comparisons_count++;
-
-// 		pairs.insert(pairs.begin(), Pair(val));
-// 		if (val > pairs[1].b)
-// 		{
-// 			pairs[0].Swap();
-// 			pairs[0].AddB(pairs[1].RemoveB());
-// 		}
-// 	}
-// 	else if (pairs.size() >= 2)
-// 	{
-// 	g_comparisons_count++;
-
-// 		if (val > pairs[1].a)
-// 			pairs.insert(pairs.begin() + 2, Pair(val));
-// 		else
-// 			pairs.insert(pairs.begin() + 1, Pair(val));
-// 	}
-// }
-
-// void sortHigh(std::vector<Pair>& pairs)
-// {
-// 	for (std::vector<Pair>::iterator it = pairs.begin(); it != pairs.end(); it++)
-// 	{
-// 		g_comparisons_count++;
-// 		if (it->a < it->b && it->a && it->b)
-// 			std::swap(it->a, it->b);
-// 	}
-// }
-
-
-// void initMainChain(std::vector<Pair>& pairs, std::vector<int>& chain)
-// {
-// 	for (std::vector<Pair>::iterator it = pairs.begin(); it != pairs.end(); it++)
-// 	{
-// 		chain.push_back(it->a);
-// 	}
-// }
-
-
-// // A binary search based function
-// // to find the position
-// // where item should be inserted
-// // in a[low..high]
-// int binarySearch(std::vector<int> vec, int item,
-// 				int low, int high)
-// {
-// 	if (high <= low)
-// 	{
-// 		g_comparisons_count++;
-// 		return (item > vec[low]) ?
-// 				(low + 1) : low;
-// 	}
-
-// 	int mid = (low + high) / 2;
-
-// 	if (item == vec[mid])
-// 		return mid + 1;
-
-// 	g_comparisons_count++;
-// 	if (item > vec[mid])
-// 		return binarySearch(vec, item,
-// 							mid + 1, high);
-// 	return binarySearch(vec, item, low,
-// 						mid - 1);
-// }
-
-// void insertElement(int item, std::vector<int>& chain)
-// {
-// 	// find location where selected should be inserted
-// 	int loc = binarySearch(chain, item, 0, std::max(chain.size() - 1, (size_t)0));
-// 	chain.insert(chain.begin() + loc, item);
-// }
-
-
-
-// void insertMainChain(std::vector<Pair>& pairs, std::vector<int>& chain)
-// {
-// 	// for (std::vector<Pair>::iterator it = pairs.begin(); it != pairs.end(); it++)
-// 	// {
-// 	// 	insertElement(it->b, chain);
-// 	// }
-// 	chain.insert(chain.begin(), pairs.begin()->b);
-// 	size_t i;
-// 	int loc, selected;
-// 	for (i = 1; i < pairs.size(); ++i)
-// 	{
-// 		selected = pairs[i].b;
-// 		if (selected < 0)
-// 			continue;
-// 		loc = binarySearch(chain, selected, 0, chain.size());
-// 		chain.insert(chain.begin() + loc, selected);
-// 	}
-// }
-
-// // // Function to sort an array a[] of size 'n'
-// // void insertionSort(std::vector<Pair>& pairs, std::vector<int>& chain)
-// // {
-// //     int i, loc, j, k, selected;
-// //     for (i = 1; i < pairs.size(); ++i)
-// //     {
-// //         j = i - 1;
-// //         selected = pairs[i].b;
-// //         // find location where selected should be inserted
-// //         loc = binarySearch(chain, selected, 0, j);
-// //         chain.insert(chain.begin() + loc, selected);
-// //     }
-// // }
-// #include "PmergeMe.hpp"
-// #define SETS 10
-// #define ELEM 9
-// int main(int argc, char **argv)
-// {
-// 	static const int sets[SETS][ELEM] = {{13, 17, 19, 6, 20, 18, 11, 8, 12}, 
-// 	{4, 10, 6, 12, 15, 14, 9, 7, 5}, 
-// 	{2, 16, 8, 10, 6, 18, 3, 1, 14}, 
-// 	{10, 2, 3, 12, 11, 5, 7, 6, 13}, 
-// 	{13, 11, 18, 14, 7, 5, 19, 1, 16}, 
-// 	{13, 14, 9, 2, 7, 5, 16, 3, 12}, 
-// 	{4, 17, 14, 16, 1, 5, 2, 13, 9}, 
-// 	{4, 19, 13, 20, 12, 6, 18, 8, 11}, 
-// 	{5, 8, 16, 3, 11, 13, 10, 17, 19}, 
-// 	{7, 19, 6, 10, 18, 5, 15, 1, 20}};
-// 	(void)argv;
-// 	(void)argc;
-// 	for (unsigned int i = 0; i < SETS; i++)
-// 	{
-// 		const int* arr = &sets[i][0];
-// 		std::vector<int> numbers(arr, arr + ELEM);
-// 		PmergeMe merge(numbers);
-// 		int comparisons = 0;
-// 		std::vector<int> sorted = merge.Sort(comparisons);
-// 		printChain(sorted);
-// 		std::cout << "Comparisons: " << comparisons << std::endl;
-// 		g_comparisons_count = 0;
-// 	}
-// }
